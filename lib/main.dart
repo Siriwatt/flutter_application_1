a84dd6073b7c9d3036e93851d7accd262b25f804
siriwatt <siriwatc66@nu.ac.th>
2025-07-31 19:08:48 +0700

    container + image

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,68 @@
 import 'package:flutter/material.dart'; // Import ไลบรารี Material ของ Flutter
- 
+
 void main() {
   // จุดเริ่มต้นของแอป Flutter
   runApp(const MyApp()); // บอก Flutter ว่าวิดเจ็ตใดคือรากฐานของแอป
 }
- 
+
 // MyApp โดยทั่วไปคือวิดเจ็ตรากฐานของแอปพลิเคชันของคุณ
 class MyApp extends StatelessWidget {
   const MyApp({super.key}); // Constructor
- 
+
   @override
   Widget build(BuildContext context) {
     // เมธอด build() ส่งคืนโครงสร้างต้นไม้วิดเจ็ตสำหรับวิดเจ็ตนี้
     return MaterialApp(
+      title: 'My Awesome App',
+      debugShowCheckedModeBanner: false, // ซ่อนแบนเนอร์ debug
       theme: ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
-    useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true, // ใช้ Material Design เวอร์ชันล่าสุด
       ),
-        title: 'Flutter Demo',
-        home: const MyHomePage()
-    );
+      home: const MyHomePage(), // วิดเจ็ตหน้าจอหลักของคุณ
+    ); // สร้าง MaterialApp ซึ่งเป็นวิดเจ็ตหลักของแอปพลิเคชัน
   }
+
+  // สร้าง MaterialApp ซึ่งเป็นวิดเจ็ตหลักของแอปพลิเคชัน
 }
- 
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // ให้โครงสร้างแอปพื้นฐาน
+    return Scaffold(
+      // ให้โครงสร้างแอปพื้นฐาน
       appBar: AppBar(
-        title: const Text('Chapter 3 Lab'),
-        backgroundColor: Colors.teal, // ปรับแต่งสี AppBar
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue, // ปรับแต่งสี AppBar
       ),
       body: Center(
-        child: Container( // กล่อง
-        padding: const EdgeInsets.all(50.0),
+        // พื้นที่เนื้อหาหลัก
+        child: Container(
+          padding: EdgeInsets.all(10.0),
           width: 300.0,
-          height: 200.0,
+          height: 150.0,
           decoration: BoxDecoration(
             color: Colors.amber,
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            borderRadius: BorderRadius.horizontal(
+              left: Radius.circular(20.0), // มุมโค้งด้านซ้าย
+              right: Radius.elliptical(50.0,20), // มุมโค้งด้านขวา
+            ),
           ),
-          child: const Text('Inside container',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.black87,
-            )
+          child: Center(
+            child: Image.asset('assets/images/dog.jpg', // ใช้ Image.asset เพื่อโหลดรูปภาพจาก assets
+            width: 100,
+            height: 50,
+            fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // การกระทำที่จะทำเมื่อกดปุ่ม
+          // ignore: avoid_print
           print('FAB Pressed!');
         },
         child: const Icon(Icons.add), // ไอคอนสำหรับปุ่ม
